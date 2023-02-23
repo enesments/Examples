c9f91976260f8ba9ae6cf402c22be8127e254e40
enesments <enesments@gmail.com>
2023-02-23 13:51:29 +0300

    OOP, Collections, Generics

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        MyList<string> names = new MyList<string>();
+            names.Add("Enes");
+           
+
+
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 10;
+            product1.ProductName = "Sandalye";
+            product1.UnitsInStock = 30;
+            product1.UnitPrice = 660;
+
+            Product product2 = new Product { Id =20, CategoryId=10,UnitsInStock=50,ProductName="Kagıt",UnitPrice =40};
+
+            //PascalCase   //CamelCase
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+        }
+    }
+}
