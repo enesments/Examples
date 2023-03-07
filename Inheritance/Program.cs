2e80c9e6e933002bf2da9fcef8653e24836947e2
Enes Menteşe <114159238+enesments@users.noreply.github.com>
2023-03-08 00:09:34 +0300

    Add files via upload

---
+++ Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+               new Customer{FirstName = "Enes"},
+               new Person {FirstName= "Ekrem"},
+               new Student {FirstName = "Eren"}
+           };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+            Console.ReadLine();
+        }
+    }
+    
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Customer : Person
+    {
+        public string City { get; set; }
+    }
+    class Student : Person
+    {
+        public string City { get; set; }
+    }
+}
